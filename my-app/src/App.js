c47050265648946277628e411c0c70a8a4bc5d70
Anna Afijan <ann.afijan@gmail.com>
2024-08-13 18:37:00 +0000

    create statefulgreeting js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
-import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
-
+import logo from './logo.svg';
+import './App.css';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <Sidebar />
+    <div className="App">
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Mike" />
     </div>
   );
 }
 
-export default App;
+export default App; 
