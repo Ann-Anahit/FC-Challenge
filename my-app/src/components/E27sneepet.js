15418006f126d290157d0339f11944078b950ebb
Anna Afijan <ann.afijan@gmail.com>
2024-08-15 07:44:55 +0000

    add eventclass and fct

---
+++ my-app/src/components/E27sneepet.js
@@ -0,0 +1,39 @@
+// import React from 'react'
+
+// function E27sneepet() {
+//   return (
+//     <div>E27sneepet</div>
+//   )
+// }
+
+// export default E27sneepet
+
+// import React from 'react'
+
+// const E27sneepet = () => {
+//   return (
+//     <div>E27sneepet</div>
+//   )
+// }
+
+// export default E27sneepet
+
+// import React, { Component } from 'react'
+
+// export class E27sneepet extends Component {
+//   render() {
+//     return (
+//       <div>E27sneepet</div>
+//     )
+//   }
+// }
+
+// export default E27sneepet
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
